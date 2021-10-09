c5ff5f0f9d2f79a90430da5e84fb694ff17fa392
harshkavatekar <71561073+harshkavatekar@users.noreply.github.com>
2021-10-09 13:21:37 +0530

    Update inst.js

--- includes/app/inst.js
+++ includes/app/inst.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    const RESTURL = 'https://institute.careerguide.com/blog/wp-json/'
+    const RESTURL = 'https://institute.careerguide.com/wp-json/'
 
     var app = {
         
